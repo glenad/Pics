8f595eddab628e95c307873367756f66b178114c
Glen <glen.adams@gmail.com>
2019-04-05 21:14:31 +0200

    Add fetchImage method and ImageList widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,8 @@
-// import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -10,19 +13,25 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+  
+    setState(() {
+      images.add(imageModel);  
+    });
+  }
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1; 
-              print('\$counter now $counter');
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text('Let\'s see some images!'),
