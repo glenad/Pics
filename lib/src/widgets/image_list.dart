87b1810afa7a92c30f925432b721b37e8c0246ed
Glen <glen.adams@gmail.com>
2019-04-07 17:08:14 +0200

    Add image and text into column with padding betw

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -23,7 +23,15 @@ class ImageList extends StatelessWidget {
       ),
       margin: EdgeInsets.all(20.0),
       padding: EdgeInsets.all(20.0),
-      child: Image.network(image.url)
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),  
+            padding: EdgeInsets.only(bottom: 8.0) 
+          ),
+          Text(image.title),
+
+      ],)
     );
   }
 
