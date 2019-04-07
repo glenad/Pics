1848aa4b24d1576303d4d507b8eddc3bfd603f9e
Glen <glen.adams@gmail.com>
2019-04-07 16:56:27 +0200

    Add buildImage  with border, margin and padding

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,8 +10,21 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        //return Image.network(images[index].url);
+        return buildImage(images[index]);
       },
     );
   }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey)
+      ),
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      child: Image.network(image.url)
+    );
+  }
+
 }
